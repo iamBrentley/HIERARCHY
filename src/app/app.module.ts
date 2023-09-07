1d9eb33dd3418618f40a88c11bbcdb136c3d4b7c
iamBrentley <brentleymuranda@gmail.com>
2023-09-07 20:19:03 +0200

    pages folder: with home, our-work, and about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
+import { OurWorkComponent } from './pages/our-work/our-work.component';
+import { AboutComponent } from './pages/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
-    HomeComponent
+    HomeComponent,
+    OurWorkComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
