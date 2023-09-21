a2244e81b9c9fba53b5e7842b0199e68f86c745c
iamBrentley <brentleymuranda@gmail.com>
2023-09-21 18:17:35 +0200

    just some stuff man, navbar there

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { OurWorkComponent } from './pages/our-work/our-work.component';
 import { AboutComponent } from './pages/about/about.component';
 import { HeaderComponent } from './header/header.component';
+
+
 
 @NgModule({
   declarations: [
