693bd07f48170b1cbca0cde8320246294b5c9870
Rivaldo Esbend <81794785+sbdDOM@users.noreply.github.com>
2023-09-25 09:45:46 +0200

    added font awesome brands

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,17 @@
+import { FactoryTarget } from '@angular/compiler';
 import { Component } from '@angular/core';
-
+import {
+  faFacebook,
+  faInstagram
+} from '@fortawesome/free-brands-svg-icons';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
+  faInstagram = faInstagram;
+  faFacebook = faFacebook;
+  
 
 }
