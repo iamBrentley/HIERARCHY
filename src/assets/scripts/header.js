99e913d31f9a634684cdca647bc060986fc01687
iamBrentley <brentleymuranda@gmail.com>
2023-09-27 18:39:59 +0200

    improvements to page

---
+++ src/assets/scripts/header.js
@@ -0,0 +1,12 @@
+window.onscroll = function() {myFunction()};
+var logo = document.getElementById("logo");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        logo.classList.add("sticky")
+    } else {
+        logo.classList.remove("sticky");
+    }
+  }
+  
